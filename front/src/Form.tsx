bbadcddd28469ababc594ccf6ed48178afb10e2c
Caius Iulius Caesar <caiusiuliuscaesar84@gmail.com>
2024-06-20 19:40:34 +0200

    MVP front

---
+++ front/src/Form.tsx
@@ -0,0 +1,165 @@
+import React, {useState} from 'react';
+import { TextField, Button, Stack, FormLabel, RadioGroup, FormControlLabel, Radio, Input, Typography, FormControlLabelProps, Box } from '@mui/material';
+import { MuiFileInput } from 'mui-file-input';
+import SendIcon from '@mui/icons-material/Send';
+import CloseIcon from "@mui/icons-material/Close";
+import styled from '@emotion/styled';
+import axios from "axios";
+import { format } from 'path';
+
+const StyledFormControlLabel = styled((props:FormControlLabelProps) => (
+  <FormControlLabel {...props} />
+))(() => ({
+  ".MuiFormControlLabel-asterisk": {
+    display: "none"
+  }
+}));
+
+const RadioGroupOther = (props:any) => {
+  return (
+    <React.Fragment>
+      <FormLabel id={props.title} required>{props.title}</FormLabel>
+      <RadioGroup
+        aria-labelledby={props.title}
+        value={props.state}
+        onChange={e => props.setState(e.target.value)}
+        sx={{mb:4}}
+      >
+        {props.choices.map((choice:any) => {
+          return <StyledFormControlLabel value={choice} control={<Radio required/>} label={choice} />
+        })}
+        <StyledFormControlLabel value="Other" control={<Radio required/>} label="Other" />
+      {(props.state === "Other") && <TextField
+          type="text"
+          variant='outlined'
+          color='secondary'
+          label="If other, please specify"
+          onChange={e => props.setOtherState(e.target.value)}
+          value={props.otherState}
+          required={props.state === "Other"}
+      />}
+      </RadioGroup>
+    </React.Fragment>
+  )
+}
+
+const RegisterForm = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [university, setUniversity] = useState('')
+
+  const [fos, setFos] = useState('')
+  const [otherFos, setOtherFos] = useState('')
+
+  const [attendance, setAttendance] = useState('')
+  const [otherAttendance, setOtherAttendance] = useState('')
+
+  const [motivation, setMotivation] = useState('')
+  
+  const [cv, setCv] = useState<File | null>(null)
+  const handleCvChange = (val:File | null) => {
+    console.log(val)
+    setCv(val)
+  }
+
+  async function handleSubmit(event:any) {
+      event.preventDefault();
+      const data = {
+        firstName, lastName, email, university, fos, otherFos, attendance, otherAttendance, motivation, cv:cv!
+      }
+      console.log(data)
+      await axios.postForm("/submit", data)
+  }
+
+  //action={<Link to="/login" />}>
+  return (
+    <React.Fragment>
+
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={2} direction="row" sx={{marginBottom: 4}}>
+          <TextField
+              type="text"
+              variant='outlined'
+              color='secondary'
+              label="First Name"
+              onChange={e => setFirstName(e.target.value)}
+              value={firstName}
+              fullWidth
+              required
+          />
+          <TextField
+              type="text"
+              variant='outlined'
+              color='secondary'
+              label="Last Name"
+              onChange={e => setLastName(e.target.value)}
+              value={lastName}
+              fullWidth
+              required
+          />
+        </Stack>
+        <TextField
+            type="email"
+            variant='outlined'
+            color='secondary'
+            label="Email"
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            fullWidth
+            required
+            sx={{mb: 4}}
+        />
+        <TextField
+            type="text"
+            variant='outlined'
+            color='secondary'
+            label="University"
+            onChange={e => setUniversity(e.target.value)}
+            value={university}
+            required
+            fullWidth
+            sx={{mb: 4}}
+        />
+
+        <RadioGroupOther title="Field of study" state={fos} setState={setFos} otherState={otherFos} setOtherState={setOtherFos} choices={
+          ["Economics", "Business Administration and Management", "Business and Finance", "Computer Science / IT", "Management and commercial engineering", "Civil/Industrial engineering", "Aeronautical engineering", "Mechanical engineering", "Electronical engineering", "Political sciences / International Relations"]
+        }/>
+        <RadioGroupOther title="Current year of attendance" state={attendance} setState={setAttendance} otherState={otherAttendance} setOtherState={setOtherAttendance} choices={
+          ["First year of Bachelor Degree", "Second year of Bachelor Degree", "Third year of Bachelor Degree", "First year of Master's Degree", "Second year of Master's Degree"]
+        }/>
+
+        <TextField
+            type="text"
+            variant='outlined'
+            multiline={true}
+            minRows={3}
+            color='secondary'
+            label="In a few words, tell us why you decided to apply for our Business Game"
+            onChange={e => setMotivation(e.target.value)}
+            value={motivation}
+            required
+            fullWidth
+            sx={{mb: 4}}
+        />
+
+        <MuiFileInput
+          label="Please attach your CV here"
+          required
+          value={cv}
+          onChange={handleCvChange}
+          fullWidth
+          sx={{mb: 4}}
+          clearIconButtonProps={{
+            title: "Remove",
+            children: <CloseIcon fontSize="small" />
+          }}
+        />
+
+        <Typography align="center"><Button variant="outlined" color="secondary" type="submit" sx={{mb:4}} endIcon={<SendIcon/>}>Apply</Button></Typography>
+      </form>
+    </React.Fragment>
+  )
+}
+ 
+export default RegisterForm;
---
+++ front/src/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Form from './Form';
+import reportWebVitals from './reportWebVitals';
+import { Box, CssBaseline, GlobalStyles, Grid, Paper, ThemeProvider, Typography, createTheme } from '@mui/material';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#B6313A',
+    },
+    secondary: {
+      main: '#485766',
+    },
+    background: {
+      default: '#B6313A'
+    }
+  },
+});
+
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <GlobalStyles
+      styles={{
+        body: { backgroundColor: "background.default" },
+      }}
+    />
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={3}>
+        <Paper elevation={4} sx={{backgroundColor:"white", pl:4, pr:4}}>
+          
+          <Box
+            component="img"
+            alt="Sant'Anna Business Game"
+            src="/LOGO-SABG-definitivo-3.png"
+            sx={{mb:4, mt:4, width:"40vw"}}
+          />
+          <Typography align="center" color="secondary"><h1>Apply for our Business Game</h1></Typography>
+          <Form />
+        </Paper>
+      </Grid>
+    </Grid>
+  </ThemeProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals(console.log);
