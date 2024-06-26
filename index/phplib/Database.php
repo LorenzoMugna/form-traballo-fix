86b181e2990613ddb01e899f19c59aa62d73dbaf
LorenzoMugna <lorenzo.mugnaiolil@gmail.com>
2024-06-26 16:09:55 +0200

    Logica submit + verifica email

--- index/phplib/Database.php
+++ index/phplib/Database.php
@@ -16,37 +16,54 @@ Table: `applications`
     cvFileName VARCHAR(255) NOT NULL
 ----------------------------------------------- */
 
-global $dbconn;
+global $dbconn, $servername, $username, $password, $dbname, $connection_up;
 $servername = "localhost";
 $username = "php";
 $password = "sabg-php-account{fiu9Vd8dRr/3nrA=}";
 $dbname = "sabg_applications";
 
+$connection_up = false;
 
 function start_connection():bool
 {
+    global $dbconn, $servername, $username, $password, $dbname, $connection_up;
+    if($connection_up)
+        return true;
+
     $dbconn = new mysqli($servername, $username, $password, $dbname);
     if($dbconn->connect_error)
         return false;
+
+    $connection_up = true;
     return true;
 }
 
+function stop_connection():bool
+{
+    global $dbconn, $connection_up;
+    if(!$connection_up)
+        return true;
 
+    $dbconn->close();
+    $connection_up = false;
+    return true;
+}
 
 function insertData(
     string $firstName,
     string $lastName,
     string $email,
+    string $verificationCode,
     string $university,
     string $fos,
     string $attendance,
     string $motivation,
     string $cvFileName
 ): bool {
-    global $dbconn;
+    global $dbconn, $servername, $username, $password, $dbname, $connection_up;
     
-    $stmt = $dbconn->prepare("INSERT INTO applications (firstName, lastName, email, university, fos, attendance, motivation, cvFileName) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssss", $firstName, $lastName, $email, $university, $fos, $attendance, $motivation, $cvFileName);
+    $stmt = $dbconn->prepare("INSERT INTO applications (firstName, lastName, email, verificationCode, university, fos, attendance, motivation, cvFileName) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssssss", $firstName, $lastName, $email, $verificationCode, $university, $fos, $attendance, $motivation, $cvFileName);
     
     if ($stmt->execute()) {
         return true;
@@ -54,4 +71,27 @@ function insertData(
         error_log("Error executing statement: " . $stmt->error, 0); 
         return false;
     }
+}
+
+function verifyEmail(string $email, string $verificationCode): int {
+    global $dbconn, $servername, $username, $password, $dbname, $connection_up;
+    
+    $stmt = $dbconn->prepare("SELECT * FROM applications WHERE email = ? AND verificationCode = ?");
+    $stmt->bind_param("ss", $email, $verificationCode);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if($result->num_rows == 0){
+        return 404;
+    }
+    
+    $stmt = $dbconn->prepare("UPDATE applications SET verified = 1 WHERE email = ? AND verificationCode = ?");
+    $stmt->bind_param("ss", $email, $verificationCode);
+    $stmt->execute();
+    
+    if($stmt->affected_rows == 0){
+        return 500;
+    }
+    
+    return 200;
 }
--- index/submit.php
+++ index/submit.php
@@ -1,24 +1,129 @@
-<html>
-<head/>
-<body>
 <?php
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+/* debug ------------------------------
 var_dump($_POST);
 echo "<br><br>";
 var_dump($_FILES);
 return;
+/*----------------------------------------- */
 
-$target_dir = "./uploads/";
-$target_file = $target_dir.basename($_POST["fileName"]);
-if(file_exists($target_file)){
-	echo "file already exists";
+
+// Check iniziali ----------------------------------------
+
+if (!isset($_POST["firstName"]) 	|| // post
+	!isset($_POST["lastName"]) 		||
+	!isset($_POST["email"]) 		||
+	!isset($_POST["university"])	||
+	!isset($_POST["fos"]) 			||
+	!isset($_POST["attendance"]) 	||
+	!isset($_POST["motivation"])
+){
+	http_response_code(400);
+	echo "Bad Request";
 	return;
 }
 
-move_uploaded_file($_FILES["fileData"]["tmp_name"], $target_file);
-echo "<p style='color: green'> File uploaded</p>"?>
-</body>
-</html>
+
+if (!isset($_FILES["cv"])){ //file
+	http_response_code(400);
+	echo "Bad Request";
+	return;
+}
+
+/* non necessario probabilmente
+if(file_exists($target_file)){
+	http_response_code(409);
+	echo "file already exists";
+	return;
+}
+/* ----------------------------------- */
+if($_FILES["cv"]["size"] > 5*1024*1024){ // file<=5MB
+	http_response_code(413);
+	echo "Request Entity Too Large";
+	return;
+}
+
+if(mime_content_type($_FILES["cv"]["tmp_name"]) != "application/pdf"){ // file pdf
+	http_response_code(400);
+	echo "Bad Request";
+	return;
+}
+
+
+// email+aggiornamento database--------------------------------------------
+require "./phplib/Database.php";
+require "./phplib/PHPMailer/PHPMailer.php";
+require "./phplib/PHPMailer/SMTP.php";
+require "./phplib/PHPMailer/Exception.php";
+
+
+$target_dir = "./application-uploads/";
+$file_name = $_POST["firstName"]."_".$_POST["lastName"]."_".(time()).".pdf";
+$target_file = $target_dir.$file_name;
+
+//Generate verification code
+$verificationCode = bin2hex(random_bytes(16));
+
+$verificationSite = "http://localhost/form-traballo/verify.php?email=".$_POST["email"]."&code=".$verificationCode;
+
+if(!start_connection()){
+	http_response_code(500);
+	echo "Internal Server Error";
+	return;
+}
+
+if(!insertData(
+	$_POST["firstName"],
+	$_POST["lastName"],
+	$_POST["email"],
+	$verificationCode,
+	$_POST["university"],
+	$_POST["fos"],
+	$_POST["attendance"],
+	$_POST["motivation"],
+	$file_name
+)){
+	http_response_code(500);
+	echo "Internal Server Error";
+	return;
+}
+
+// Send verification email ----------------------------------------------
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->isSMTP();
+
+$mail->Host = 'mail.jebesantanna.it';
+$mail->Port = 587;
+$mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
+$mail->SMTPAuth = true;
+
+$mail->Username = 'prova2_noreply@jebesantanna.it';
+$mail->Password = 'pallesudate';
+
+$mail->setFrom('prova2_noreply@jebesantanna.it', 'Business Game');
+$mail->isHTML(true);
+$mail->CharSet = 'UTF-8';
+
+$mail->addAddress($_POST["email"]);
+$mail->Subject = "Please verify your email address for Sant'Anna Business Game";
+$mail->Body = "
+<h2>Click on 'Verify' to verify your email address</h2><br>
+<a href='".$verificationSite."'>Verify</a>
+";
+
+if (!$mail->send()) { http_response_code(500);
+	echo "Internal Server Error";
+	return;
+}
+// -----------------------------------------------------------------------
+
+
+// Upload file -----------------------------------------------------------
+move_uploaded_file($_FILES["cv"]["tmp_name"], $target_file);
+http_response_code(200);
+
+
+stop_connection();
